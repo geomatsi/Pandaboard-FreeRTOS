16e79af2a0251f166c6df15543adfd1d7e84f220
matsi <matsi@example.org>
2014-08-14 08:01:03 -0400

    Add new trace functions
    - trace_printf_len: write specified amount of symbols from string to trace buffer
    - trace_append: write format string to trace buffer
    
    Signed-off-by: matsi <matsi@example.org>

--- include/trace.h
+++ include/trace.h
@@ -3,5 +3,6 @@
 
 void trace_printf(char *msg);
 void trace_value(unsigned int register_value);
+int trace_append(const char *format, ...);
 
 #endif /* __TRACE_H__ */
--- src/trace.c
+++ src/trace.c
@@ -1,6 +1,8 @@
 /* trace feature for rpmsg/remoteproc */
 
 #include <string.h>
+#include <stdint.h>
+#include <stdarg.h>
 
 #define ASCII_MASK 0x0000000F
 
@@ -16,6 +18,132 @@ volatile char buf[TRACE_BUF_SIZE];
 
 /* */
 
+static int printchar(int c)
+{
+    int pc = 0;
+
+	if (((unsigned int) trace_buf) >= (TRACE_BUF_BASE + TRACE_BUF_SIZE))
+		trace_buf = (char *)TRACE_BUF_BASE;
+
+    *trace_buf++ = c;
+    pc += 1;
+
+    return pc;
+}
+
+static int printstr(const char *string)
+{
+	int pc = 0;
+
+	for ( ; *string ; ++string) {
+		pc += printchar(*string);
+	}
+
+	return pc;
+}
+
+static int printint(int i, int b, int sign, int letbase)
+{
+	uint32_t u, mu;
+	int t;
+
+	int pc = 0;
+
+	if (i == 0) {
+		pc += printchar('0');
+		goto done;
+	}
+
+	if (sign && b == 10 && i < 0) {
+		pc += printchar('-');
+		u = -i;
+	} else {
+		u = i;
+	}
+
+	mu = 1;
+
+	while (1) {
+		if (u/mu < b)
+			break;
+
+		mu *= b;
+	}
+
+	while (mu) {
+		t = u / mu;
+
+		if( t >= 10 )
+			t += letbase - '0' - 10;
+		pc += printchar(t + '0');
+
+		u = u % mu;
+		mu /= b;
+	}
+
+done:
+	return pc;
+}
+
+static int printvarg(int *varg)
+{
+	char scr[2];
+
+	char *format = (char *)(*varg++);
+	int pc = 0;
+
+	for (; *format != 0; ++format) {
+		if (*format == '%') {
+			++format;
+			if (*format == '\0')
+				break;
+
+			if (*format == '%')
+				goto symbol;
+
+			if( *format == 's' ) {
+				char *s = *((char **)varg++);
+				pc += printstr(s ? s : "(null)");
+				continue;
+			}
+
+			if( *format == 'd' ) {
+				pc += printint(*varg++, 10, 1, 'a');
+				continue;
+			}
+
+			if( *format == 'x' ) {
+				pc += printint(*varg++, 16, 0, 'a');
+				continue;
+			}
+
+			if( *format == 'X' ) {
+				pc += printint(*varg++, 16, 0, 'A');
+				continue;
+			}
+
+			if( *format == 'u' ) {
+				pc += printint(*varg++, 10, 0, 'a');
+				continue;
+			}
+
+			if( *format == 'c' ) {
+				scr[0] = *varg++;
+				scr[1] = '\0';
+				pc += printstr(scr);
+				continue;
+			}
+		} else {
+		symbol:
+			pc += printchar(*format);
+		}
+	}
+
+	return pc;
+}
+
+/* */
+
 void trace_printf(char *msg)
 {
 	/* TODO: for now, trace will reset when size exceed the buffer size.
@@ -23,6 +151,18 @@ void trace_printf(char *msg)
 
 	unsigned int len;
 	len = strlen(msg);
+
+	if ((len + (unsigned int)trace_buf) > (TRACE_BUF_BASE + TRACE_BUF_SIZE))
+		trace_buf = (char *)TRACE_BUF_BASE;
+
+	strncpy(trace_buf, msg, len);
+	trace_buf += len;
+}
+
+void trace_printf_len(char *msg, int len)
+{
+	/* TODO: for now, trace will reset when size exceed the buffer size.
+	 * we need to keep the history as it is done in bios. */
 
 	if ((len + (unsigned int)trace_buf) > (TRACE_BUF_BASE + TRACE_BUF_SIZE))
 		trace_buf = (char *)TRACE_BUF_BASE;
@@ -50,4 +190,11 @@ void trace_value(unsigned int register_value)
 
 	trace_printf(string);
 	trace_printf("\n");
+}
+
+int trace_append(const char *format, ...)
+{
+	int *varg = (int *)(&format);
+
+	return printvarg(varg);
 }
